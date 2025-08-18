9753fe1cec7f59ee0a77c21d5c717ad516138e93
melnicka <ma.melnicka@gmail.com>
2025-08-18 20:03:39 +0200

    feat: add 2 utility functions

--- src/bin/testing.rs
+++ src/bin/testing.rs
@@ -2,53 +2,12 @@ use seth::neat::genome::*;
 use std::collections::HashMap;
 
 fn main() {
-    let n0 = Node {
-        id: 0,
-        node_type: NodeType::Input,
-    };
-
-    let n1 = Node {
-        id: 1,
-        node_type: NodeType::Input,
-    };
-
-    let n2 = Node {
-        id: 2,
-        node_type: NodeType::Output,
-    };
-
-    let c0 = Connection {
-        id: ConnectionID {
-            in_node_id: n0.id,
-            out_node_id: n2.id,
-        },
-        weight: 1.0,
-        enabled: true,
-    };
-
     let mut dupa = InnovationHistory {
         history: HashMap::new(),
         counter: 0,
     };
 
-    let mut g1 = Genome {
-        num_inputs: 1,
-        num_outputs: 1,
-        total_nodes: 2,
-        nodes: vec![n0.clone(), n1.clone(), n2.clone()],
-        connections: Vec::new(),
-        fitness: 0.0,
-    };
-
-    let mut g2 = Genome {
-        num_inputs: 1,
-        num_outputs: 1,
-        total_nodes: 2,
-        nodes: vec![n0, n1, n2],
-        connections: Vec::new(),
-        fitness: 0.0,
-    };
-
-    let (id1, id2) = g1.get_valid_node_ids();
-    println!("{}, {}", id1, id2)
+    let g = Genome::new(1, 2, &mut dupa);
+    println!("{:?}", g)
+ 
 }
--- src/neat/genome.rs
+++ src/neat/genome.rs
@@ -1,6 +1,8 @@
+use piston_window::Input;
 use rand::Rng;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::process::Output;
 use std::rc::Rc;
 use rand::prelude::IndexedRandom;
 
@@ -13,14 +15,14 @@ pub enum NodeType {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Node {
-    pub id: u32,
+    pub id: i32,
     pub node_type: NodeType,
 }
 
 #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
 pub struct ConnectionID {
-    pub in_node_id: u32,
-    pub out_node_id: u32,
+    pub in_node_id: i32,
+    pub out_node_id: i32,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -32,8 +34,8 @@ pub struct Connection {
 
 #[derive(Debug, PartialEq)]
 pub struct InnovationHistory {
-    pub history: HashMap<ConnectionID, u32>,
-    pub counter: u32,
+    pub history: HashMap<ConnectionID, i32>,
+    pub counter: i32,
 }
 
 #[derive(Debug, PartialEq)]
@@ -47,7 +49,7 @@ pub struct Genome {
 }
 
 impl InnovationHistory {
-    pub fn get_conn_innovation(&mut self, conn_id: ConnectionID) -> u32 {
+    pub fn get_conn_innovation(&mut self, conn_id: ConnectionID) -> i32 {
         let inno = self.history.get(&conn_id);
 
         match inno {
@@ -63,24 +65,64 @@ impl InnovationHistory {
 }
 
 impl Genome {
-    //     // creates a new network with
-    //     pub fn create_new<'a>(num_inputs: i32,
-    //         num_outputs: i32,
-    //         ih: &'a InnovationHistory) -> Genome<'a> {
+         // creates a new network with
+         pub fn new(num_inputs: i32,
+            num_outputs: i32,
+            ih: &mut InnovationHistory) -> Genome {
 
-    //     }
+            let mut in_node_ids:Vec<i32> = Vec::new();
+            let mut out_node_ids:Vec<i32> = Vec::new();
+
+            let mut rng = rand::rng();
+
+            let mut g = Genome {
+                num_inputs,
+                num_outputs,
+                total_nodes: 0,
+                nodes: Vec::new(),
+                connections: Vec::new(),
+                fitness: 0.0
+            };
+
+            for i in 0..num_inputs {
+                in_node_ids.push(g.total_nodes);
+                g.add_node(g.total_nodes, NodeType::Input);
+            };
+
+            for i in 0..num_outputs {
+                out_node_ids.push(g.total_nodes);
+                g.add_node(g.total_nodes, NodeType::Output);
+            };
+
+            for in_id in &in_node_ids {
+                for out_id in &out_node_ids {
+                    let mut weight: f64 = rng.random_range(-1.0..1.0);
+                    g.add_connection(ih, *in_id, *out_id, weight);
+                };
+            };
+            g
+         }
+
+    pub fn add_node(&mut self, node_id: i32, node_type: NodeType) {
+        let new_node = Node {
+            id: node_id,
+            node_type
+        };
+        self.nodes.push(new_node);
+        self.total_nodes +=1;
+    }
 
     pub fn add_connection(&mut self, 
         ih: &mut InnovationHistory, 
-        in_node_id: u32, out_node_id:
-        u32,
+        in_node_id: i32, out_node_id:
+        i32,
         weight: f64) {
         let conn_id = ConnectionID {
             in_node_id,
             out_node_id,
         };
         if !self.connection_exist(conn_id) {
-            let _inno: u32 = ih.get_conn_innovation(conn_id);
+            let _inno: i32 = ih.get_conn_innovation(conn_id);
             let mut conn = Connection {
                 id: conn_id,
                 weight,
@@ -96,7 +138,7 @@ impl Genome {
 
 
 
-    pub fn get_valid_node_ids(&self) -> (u32, u32) {
+    pub fn get_valid_node_ids(&self) -> (i32, i32) {
         let mut rng = rand::rng();
 
         loop {
